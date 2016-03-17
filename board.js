78b42fcd24bce899509453543f2382a3f33a2aa3
Zeng Xi <apibrain@gmail.com>
2016-03-17 14:57:07 +0800

    add keyup function

--- board.js
+++ board.js
@@ -36,13 +36,17 @@ function contains_chinese(str) {
     return reg.test(str);
 }
 
-
- // 限制文本框只允许输入整数
- $(document).on('keydown', '.integer_required', function(e) {
-	// Allow: backspace, delete, tab, escape and enter
-	// Allow: Ctrl+A
-	// Allow: home, end, left, right
-	if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 || (e.keyCode == 65 && e.ctrlKey === true) || (e.keyCode >= 35 && e.keyCode <= 39)) {
+/*******************************************************
+                    结合 jQuery 操作
+ *******************************************************/
+/**
+ * 限制文本框只允许输入整数
+ */
+$(document).on('keydown', '.integer_required', function(e) {
+    // Allow: backspace, delete, tab, escape and enter
+    // Allow: Ctrl+A
+    // Allow: home, end, left, right
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 || (e.keyCode == 65 && e.ctrlKey === true) || (e.keyCode >= 35 && e.keyCode <= 39)) {
         // let it happen, don't do anything
     	return;
     }
@@ -50,5 +54,15 @@ function contains_chinese(str) {
     // Ensure that it is a number and stop the keypress
     if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
         e.preventDefault();
+    }
+});
+
+/**
+ * 按回车提交（或执行其他方法）
+ */
+$('#input').keyup(function(e) {
+    if (e.keyCode == '13') {
+        $('form').submit();
+        // or do something...
     }
 });
