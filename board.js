8b4a1666abbc478e4adc280e2003b8f85f032571
Zeng Xi <apibrain@gmail.com>
2016-03-23 09:56:48 +0800

    添加了 get_random_number(min, max) 方法

--- board.js
+++ board.js
@@ -36,6 +36,33 @@ function contains_chinese(str) {
     return reg.test(str);
 }
 
+
+/*******************************************************
+                    一些常用方法
+ *******************************************************/
+/**
+ * 获取指定范围内的随机数
+ * @param  {int} min 最小数值
+ * @param  {int} max 最大数值
+ * @return {int}     min 和 max 之间的随机数
+ *
+ */
+function get_random_number(min, max) {
+    // 如果最小值等于最大值，则直接返回最小值
+    if (min == max) {
+        return min;
+    }
+
+    // 如果最小值大于最大值，则返回false
+    if (min > max) {
+        return false;
+    }
+
+    var range = max - min;
+    var random = Math.random();
+    return min + Math.round(random * range);
+}
+
 /*******************************************************
                     结合 jQuery 操作
  *******************************************************/
@@ -46,7 +73,9 @@ $(document).on('keydown', '.integer_required', function(e) {
     // Allow: backspace, delete, tab, escape and enter
     // Allow: Ctrl+A
     // Allow: home, end, left, right
-    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 || (e.keyCode == 65 && e.ctrlKey === true) || (e.keyCode >= 35 && e.keyCode <= 39)) {
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
+        (e.keyCode == 65 && e.ctrlKey === true) ||
+        (e.keyCode >= 35 && e.keyCode <= 39)) {
         // let it happen, don't do anything
     	return;
     }
